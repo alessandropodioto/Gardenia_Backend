7c654f9d000c717b9850c2e8e2fc8e0b174e0074
alessandropodioto <alessandro.podio.to@gmail.com>
2026-03-16 16:19:07 +0100

    prima commit

---
+++ src/test/java/com/betacom/pr/GardeniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
