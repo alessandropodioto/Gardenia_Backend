38810f5ced9ec657ce3cfed85965f572eb22ea09
Kristian979 <kristian_preci99@live.com>
2026-03-18 17:46:32 +0100

    Status implementation

--- src/main/java/com/betacom/pr/services/interfaces/IStatusServices.java
+++ src/main/java/com/betacom/pr/services/interfaces/IStatusServices.java
@@ -1,5 +1,12 @@
 package com.betacom.pr.services.interfaces;
 
+import java.util.List;
+import com.betacom.pr.dto.outputs.StatusDTO;
+
 public interface IStatusServices {
+
+    List<StatusDTO> getAll();
+    
+    StatusDTO getById(Integer id);
 
 }
