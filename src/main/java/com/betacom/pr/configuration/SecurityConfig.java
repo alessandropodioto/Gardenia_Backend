5b64bdd2481d950b58510be2e3d9cb3a03798355
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2026-03-23 11:04:49 +0100

    security config

--- src/main/java/com/betacom/pr/configuration/SecurityConfig.java
+++ src/main/java/com/betacom/pr/configuration/SecurityConfig.java
@@ -1,5 +1,32 @@
 package com.betacom.pr.configuration;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+public class SecurityConfig {
+    @Bean
+	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+			http.authorizeHttpRequests((requests) -> requests
+					.requestMatchers("/admin", "/admin/**").hasRole("ADMIN")
+					.requestMatchers("/", "/address", "/category", "/image", "/product", "/shoppingCart", "/status", "/subcategory", "/user", "/order").permitAll()
+					.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+					.loginPage("/login")
+					.permitAll()
+					)
+			.logout((logout) -> logout.permitAll());
+			
+			return http.build();
+		}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
