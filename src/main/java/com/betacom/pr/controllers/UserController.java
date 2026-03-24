5d74af1751909a32b31f9a2fa90033e0774c4c40
EssieDyva <st3f1703@gmail.com>
2026-03-24 17:41:56 +0100

    added temporary crossorigin bean

--- src/main/java/com/betacom/pr/controllers/UserController.java
+++ src/main/java/com/betacom/pr/controllers/UserController.java
@@ -2,15 +2,7 @@ package com.betacom.pr.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.betacom.pr.dto.inputs.LoginReq;
 import com.betacom.pr.dto.inputs.UserReq;
@@ -20,6 +12,7 @@ import com.betacom.pr.services.interfaces.IUserServices;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("rest/user")
